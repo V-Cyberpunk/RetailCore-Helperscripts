32726ca52e77c3a44c6a7726daba562b2b839fed
Avatar Korra <44802344+V-Cyberpunk@users.noreply.github.com>
2024-02-07 19:59:46 +0100

    Add files via upload

--- dbextract.ps1
+++ dbextract.ps1
@@ -7,9 +7,11 @@ Start-Sleep -s 5
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c mysqldump.exe -u root -padmin hotfixes > hotfixes.sql" -Wait
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c mysqldump.exe -u root -padmin world > world.sql" -Wait
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c mysqldump.exe -u root -padmin --no-data --no-create-info --no-create-db --no-set-names --skip-comments --skip-dump-date --skip-opt --triggers characters > characters_trigger.sql" -Wait
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c mysqldump.exe -u root -padmin --no-data --no-create-info --no-create-db --no-set-names --skip-comments --skip-dump-date --skip-opt --skip-triggers --events characters > characters_events.sql" -Wait
 move hotfixes.sql $PSScriptRoot\
 move world.sql $PSScriptRoot\
 move characters_trigger.sql $PSScriptRoot\
+move characters_events.sql $PSScriptRoot\
 taskkill /F /IM mysqld.exe
 cd $PSScriptRoot
 Remove-Item -Path $PSScriptRoot\PreCompiledCore -Recurse -Force
@@ -18,7 +20,7 @@ Remove-Item -Path $PSScriptRoot\*PreCompiledCore.zip
 $date = Get-Date -Format "yyyy-MM-dd"
 
 $compress = @{
-  Path = "hotfixes.sql", "world.sql", "characters_trigger.sql"
+  Path = "hotfixes.sql", "world.sql", "characters_trigger.sql", "characters_events.sql"
   CompressionLevel = "Optimal"
   DestinationPath = "RetailCoreDB_" + $date + "_tc-" + $tc
 }
